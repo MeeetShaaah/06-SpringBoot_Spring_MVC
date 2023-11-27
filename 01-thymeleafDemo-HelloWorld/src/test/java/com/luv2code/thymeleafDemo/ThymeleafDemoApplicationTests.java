80635639b10f66cbf826c06c5e4c476bfaad5df7
Meet Shah <meetshah7199@gmail.com>
2023-11-27 16:15:58 -0500

    Start With Spring MVC...

---
+++ 01-thymeleafDemo-HelloWorld/src/test/java/com/luv2code/thymeleafDemo/ThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.thymeleafDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
