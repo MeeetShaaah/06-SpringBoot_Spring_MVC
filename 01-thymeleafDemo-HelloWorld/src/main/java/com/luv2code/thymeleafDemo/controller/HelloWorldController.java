d139528980ac25116549d0d284e914542e6bc1d3
Meet Shah <meetshah7199@gmail.com>
2023-11-28 00:30:55 -0500

    Coading for Read data.

--- 01-thymeleafDemo-HelloWorld/src/main/java/com/luv2code/thymeleafDemo/controller/HelloWorldController.java
+++ 01-thymeleafDemo-HelloWorld/src/main/java/com/luv2code/thymeleafDemo/controller/HelloWorldController.java
@@ -1,2 +1,20 @@
-package com.luv2code.thymeleafDemo.controller;public class HelloWorldController {
+package com.luv2code.thymeleafDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    //controller method to show initial form
+    @RequestMapping("/showForm")
+    public String showform(){
+        return "hello-world-form";
+    }
+
+   //controller method to process the form
+   @RequestMapping("/processForm")
+   public String processForm(){
+       return "helloworld";
+   }
 }
