90f85fe642028058fe8ec11fb231d3ba058b536d
Meet Shah <meetshah7199@gmail.com>
2023-11-30 12:46:27 -0500

    change to wrapper class of int for free passes field

--- 01-thymeleafDemo-HelloWorld/src/main/java/com/luv2code/thymeleafDemo/model/Student.java
+++ 01-thymeleafDemo-HelloWorld/src/main/java/com/luv2code/thymeleafDemo/model/Student.java
@@ -20,15 +20,16 @@ public class Student {
     private List<String> favoriteSystems;
 
     // Min max validation ruls for freepasses
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or equal to zero..!!")
     @Max(value = 10, message = "must be less than or equal to ten..!!")
-    private int freePasses;
+    private Integer freePasses;
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
